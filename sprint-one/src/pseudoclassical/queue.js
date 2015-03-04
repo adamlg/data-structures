1e13a56f35229cb4a2a7b9d6e8624e0a54af2893
MKS-Student <mks.student@hackreactor.com>
2015-03-04 14:46:04 -0800

    finished all 4

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,24 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+	this.line = {}
+	this.nextInLine = 0
+	this.endOfLine = 0
 };
 
+Queue.prototype.enqueue = function(val) {
+	this.line[this.endOfLine] = val
+	this.endOfLine++
+}
+
+Queue.prototype.dequeue = function() {
+	var removed = this.line[this.nextInLine]
+	delete this.line[this.nextInLine]
+	if(this.nextInLine < this.endOfLine) {
+		this.nextInLine++
+	}
+	return removed
+}
+
+Queue.prototype.size = function() {
+	return this.endOfLine - this.nextInLine
+}
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,23 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+	this.line = {}
+	this.span = 0;
 };
+
+Stack.prototype.push = function(val) {
+	this.line[this.span] = val
+	this.span++
+}
+
+Stack.prototype.pop = function() {
+	this.span && this.span--
+	var removed = this.line[this.span]
+	delete this.line[this.span]
+	return removed
+}
+
+Stack.prototype.size = function() {
+	return this.span
+}
+
 
 
