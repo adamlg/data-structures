6922fe98fbe0afe0b429d3e01588333ac63123a3
MKS-Student <mks.student@hackreactor.com>
2015-03-05 14:57:33 -0800

    finished with trees

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,33 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = new Node(value) //create new node with node.value, but node.next is null
+    if (this.head === null) {
+      this[value] = newNode
+      this.head = newNode            //if there is no existing head, the node added is the head
+      this.tail = newNode
+    } 
+    else {
+      this[value] = newNode
+      var oldTail = this[this.tail.value]    //find the object that used to be the tail
+      oldTail.next = value                  //change the old tail's next value to point to our new tail
+      this.tail = newNode             //redefine list.tail
+    }
   };
 
   list.removeHead = function(){
+    var removed = this.head
+    console.log(removed)
+    this.head = this[removed.next]
+    delete this[removed.value]
+    return removed.value
   };
 
   list.contains = function(target){
+    if (this[target]) {
+      return true
+    } 
+    else return false
   };
 
   return list;
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,27 +1,53 @@
 var Tree = function(value){
   var newTree = {};
+  newTree = Object.create(treeMethods)
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
 
-
-
-
-
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+	var newNode = Tree(value)
+	this.children.push(newNode)
 };
 
-treeMethods.contains = function(target){
+treeMethods.contains = function(target) {
+	var boo = false
+	var search = function(node) {
+		for (var i = 0; i < node.children.length; i++) {
+			if (node.children[i].value === target) {
+				console.log(node.children[i])
+				boo = true
+			} 
+			else {
+				if (node.children[i]) {
+					search(node.children[i])
+				}
+			}
+		}
+	}
 
+	search(this)
+	return boo
+}
+
+/*	if (this.value === target) {
+		return true
+	} 
+	else return false
+	// else {
+	// 	if (this.hasChildNodes) {
+	// 		return this.children.forEach(this.contains(target))
+	// 	} else {
+	// 		return false
+	// 	}
+	// }
 };
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
