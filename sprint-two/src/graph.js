436286cbcd6c0cb32d95309f6b01cd0173a12cda
MKS-Student <mks.student@hackreactor.com>
2015-03-06 10:15:19 -0800

    finished graph and set

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,31 +1,76 @@
 
 
 var Graph = function(){
+	var obj = Object.create(Graph.prototype)
+	obj.nodes = {}
+	return obj
 };
 
 Graph.prototype.addNode = function(node){
+	this.nodes[node] = {"value":node,"edges":[]}
 };
 
 Graph.prototype.contains = function(node){
+	if (this.nodes[node]) {
+		return true
+	}
+	else return false
 };
 
 Graph.prototype.removeNode = function(node){
+	delete this.nodes[node]
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+	var check = this.nodes[fromNode]
+	var correct = false
+	for (var i = 0; i < check.edges.length; i++) {
+		if (check.edges[i] === toNode) {
+			correct = true
+		}
+	}
+	return correct
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+	var from = this.nodes[fromNode]
+	var to = this.nodes[toNode]
+	from.edges.push(toNode)
+	to.edges.push(fromNode)
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+	var from = this.nodes[fromNode]
+	var to = this.nodes[toNode]
+
+	for (var i = 0; i < from.edges.length; i++) {
+		if(from.edges[i] === toNode) {
+			delete from.edges[i]
+		}
+	}
+	for (var j = 0; j < to.edges.length; j++) {
+		if(to.edges[j] === fromNode) {
+			delete to.edges[j]
+		}
+	}
 };
 
 Graph.prototype.forEachNode = function(cb){
+	var nodes = this.nodes
+	for (var i in nodes) {
+		cb.call(null,nodes[i].value)
+	}
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+
+	removeNode: O(1)
+	hasEdge: O(N)
+	addEdge: O(1)
+	removeEdge: O(N)
+	forEachNode: O(N)
+
  */
 
 
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,38 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+	this._storage.push(item)
 };
 
 setPrototype.contains = function(item){
+	var boo = false
+	for (var i = 0; i < this._storage.length; i++) {
+		if (this._storage[i] === item) {
+			boo = true
+		}
+	}
+	return boo
 };
 
 setPrototype.remove = function(item){
+	for (var i = 0; i < this._storage.length; i++) {
+		if(this._storage[i] === item) {
+			delete this._storage[i]
+		}
+	}
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+
+	add: O(1)
+	contains: O(N)
+	remove: O(N)
+
  */
