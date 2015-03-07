68ae8ad455aad571f87a9a41964427d21dbb9131
MKS-Student <mks.student@hackreactor.com>
2015-03-07 08:13:52 -0800

    finished bst

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,16 +1,9 @@
 var BinarySearchTree = function(value){
 	var newBst = {};
 	newBst = Object.create(bstMethods)
-	/*var firstNode = {}
-	firstNode.left = []
-	firstNode.right = []
-	firstNode.value = value
-	newBst.storage = [firstNode]; 
-	*/
-	newBst.left = []
-	newBst.right = []
+	newBst.left = {}
+	newBst.right = {}
 	newBst.value = value
-
 	return newBst;
 };
 
@@ -22,77 +15,60 @@ bstMethods.insert = function(value) {
 	var newNode = BinarySearchTree(value)
 
 	var search = function (node) {				
-		if (value > node.value) {	
-		console.log(node.right)			//compare newnode value with firstnode value
-			if (node.right.length) {			//look at the right		
-				search(node.right/*[0]*/)			//recurse on right node if it already exists
+		if (value > node.value) {						//compare newnode value with firstnode value		
+			if (node.right.hasOwnProperty("value")) {	//look at the right		
+				search(node.right)						//recurse on right node if it already exists
 			}
-			else node.right.push(newNode/*.storage[0]*/)		//if right node doesn't exist, push newnode there				
+			else node.right = newNode					//if right node doesn't exist, push newnode there				
 		}
-		else if (value < node.value) {			//look to the left
-			console.log(node.left)
-			if (node.left.length) {
-				search(node.left/*[0]*/)
+		else if (value < node.value) {					//look to the left
+			if (node.left.hasOwnProperty("value")) {
+				search(node.left)
 			}
-			else node.left.push(newNode/*.storage[0]*/)
+			else node.left = newNode
 		}				
 	}
-
-	//search(this.storage[0])
 	search(this)
-
-	//function we use for recursion
-
-	//check if any nodes exist
-	// if (!this.storage.length) {		//if no nodes exist, this will be the first node
-	// 	this.storage.push(newNode)
-	// 	return
-	// }
-	
-	// var search = function(value) {		//function starts here
-	// 	if (value < this.value) {
-	// 		if (this.left.length === 0) {
-	// 			this.left.push(newNode)
-	// 			this.left.children.push(newNode)
-	// 		} else {
-	// 			search(this.left)
-	// 		}
-	// 	} else if (value > this.value) {
-	// 		if (this.right.length === 0) {
-	// 			this.right.push(newNode)
-	// 			this.right.children.push(newNode)
-	// 		} else {
-	// 			search(this.right)
-	// 		}
-	// 	} else {
-	// 		return "This value is already in the tree."
-	// 	}
-		
-	// }
-
-	// search(newBst.children)
-
-	// }
-	// //if nodes exist, check value against node value
-	// //if value<existing, node, recurse this function on left node, else recurse on right node
-	// //once there are no more left or right children, add value as left or right on final node
-
-	// if (value > this.value && this.children.length !== 0) {
-	// 	this.right = newNode
-	// }
-	// else this.left = newNode
 }
 
 bstMethods.contains = function(target) {
-
+	var boo = false
+	var search = function(node) {
+		if (target === node.value) {
+			return boo = true
+		}
+		else if (target > node.value) {					//look to right
+			search(node.right)
+		}
+		else if (target < node.value) {					//look to left
+			search(node.left)
+		}
+	}
+	search(this)
+	return boo
 }
 
 bstMethods.depthFirstLog = function(cb) {
-
+	var cbSearch = function(node) {
+		cb.call(null, node.value)
+		
+		if (node.left.value) {
+			cbSearch(node.left)
+		}
+		if (node.right.value) {
+			cbSearch(node.right)
+		}
+	}
+	return cbSearch(this)
 }
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+
+	insert: O(log(n))
+	contains: O(log(n))
+	depthFirstLog: O(log(n))
+
  */
